f66829d49199713e2b0f3e6486ac2c0237a09baf
Hannes Ovrén <hannes.ovren@liu.se>
2017-11-28 14:56:33 +0100

    Move things around, update gitignore

--- tests/module_test.py
+++ tests/module_test.py
@@ -2,9 +2,10 @@ import pytest
 
 import numpy as np
 
-import pymodule.python_example as m
+import taser.python_example as m
 
 def test_vector_add():
+    print(m)
     a = np.array([1., 2, 3])
     b = np.array([2., 3, 1])
     c = m.vector_add(a, b)
