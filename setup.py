38cc3e59a64352b558a00f15aa06e941ec8d21f6
Hannes Ovrén <hannes.ovren@liu.se>
2017-11-28 15:25:36 +0100

    Remove Catch C++ test runner since we don't use it

--- setup.py
+++ setup.py
@@ -67,29 +67,6 @@ class CMakeBuild(build_ext):
                               cwd=self.build_temp)
         print()  # Add an empty line for cleaner output
 
-class CatchTestCommand(TestCommand):
-    """
-    A custom test runner to execute both python unittest tests and C++ Catch-
-    lib tests.
-    """
-    def distutils_dir_name(self, dname):
-        """Returns the name of a distutils build directory"""
-        dir_name = "{dirname}.{platform}-{version[0]}.{version[1]}"
-        return dir_name.format(dirname=dname,
-                               platform=sysconfig.get_platform(),
-                               version=sys.version_info)
-
-    def run(self):
-        # Run python tests
-        super(CatchTestCommand, self).run()
-        print("\nPython tests complete, now running C++ tests...\n")
-        # Run catch tests
-        subprocess.call(['./*_test'],
-                        cwd=os.path.join('build',
-                                         self.distutils_dir_name('temp')),
-                        shell=True)
-
-
 setup(
     name='python_cpp_example',
     version='0.1',
@@ -98,6 +75,6 @@ setup(
     description='A hybrid Python/C++ test project',
     long_description='',
     ext_modules=[CMakeExtension('taser/python_example')],
-    cmdclass=dict(build_ext=CMakeBuild, test=CatchTestCommand),
+    cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
 )
