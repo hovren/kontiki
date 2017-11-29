300712d9efb5ceb545c5f0c6d42fd2b2da93b66d
Hannes Ovrén <hannes.ovren@liu.se>
2017-11-29 09:01:44 +0100

    Rename dummy tests

--- tests/dummy_test.py
+++ tests/dummy_test.py
@@ -2,10 +2,12 @@ import pytest
 
 import numpy as np
 
+import taser
 import taser.dummy as m
 
 def test_vector_add():
     print(m)
+    print(taser)
     a = np.array([1., 2, 3])
     b = np.array([2., 3, 1])
     c = m.vector_add(a, b)
