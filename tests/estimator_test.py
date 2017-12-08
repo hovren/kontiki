64515541a7a02b69715a0cef3eb27d85f9ecf976
Hannes Ovrén <hannes.ovren@liu.se>
2017-12-08 14:40:22 +0100

    Create (local) estimator fixture

--- tests/estimator_test.py
+++ tests/estimator_test.py
@@ -4,21 +4,30 @@ import numpy as np
 
 from taser.trajectory_estimator import TrajectoryEstimator
 
-def test_create(trajectory):
+@pytest.fixture
+def estimator(trajectory):
+    return TrajectoryEstimator(trajectory)
+
+def test_same_trajectory(trajectory):
     "Test that we can create estimators for all trajectory types"
     estimator = TrajectoryEstimator(trajectory)
     assert estimator.trajectory is trajectory
 
 
-def _test_add_measurement(trajectory, measurements):
-    estimator = TrajectoryEstimator(trajectory)
+def test_solve(estimator):
+    summary = estimator.solve()
+    print(summary.FullReport())
+    assert summary.num_parameters == 0
+
+
+def _test_add_measurement(estimator, measurements):
     for m in measurements:
         estimator.add_measurement(m)
 
 
-def test_add_camera_measurement(trajectory, camera_measurements):
-    _test_add_measurement(trajectory, camera_measurements)
+def test_add_camera_measurement(estimator, camera_measurements):
+    _test_add_measurement(estimator, camera_measurements)
 
 
-def test_add_simple_measurements(trajectory, simple_measurements):
-    _test_add_measurement(trajectory, simple_measurements)
+def test_add_simple_measurements(estimator, simple_measurements):
+    _test_add_measurement(estimator, simple_measurements)
