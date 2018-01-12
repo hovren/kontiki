269b42d44d4040b6ae4c2b641ce8cd8adcd032d9
Hannes Ovrén <hannes.ovren@liu.se>
2018-01-12 18:38:26 +0100

    Estimating position with R3Spline compiles and runs (no tests!)

--- include/taser/trajectories/uniform_r3_spline_trajectory.h
+++ include/taser/trajectories/uniform_r3_spline_trajectory.h
@@ -49,30 +49,81 @@ class SplineViewBase : public ViewBase<T, Meta> {
   // Inherit constructor
   using ViewBase<T, Meta>::ViewBase;
 
-  double t0() const {
-    return this->meta_.t0;
+  T t0() const {
+    return T(this->meta_.t0);
   }
 
-  double dt() const {
-    return this->meta_.dt;
+  T dt() const {
+    return T(this->meta_.dt);
   }
 
   size_t NumKnots() const {
     return this->meta_.n;
   }
+
+  void CalculateIndexAndInterpolationAmount(T t, int& i0, T& u) const {
+    T s = (t - t0()) / dt();
+    i0 = PotentiallyUnsafeFloor(s);
+    u = s - T(i0);
+  }
+
+ protected:
+  int PotentiallyUnsafeFloor(double x) const {
+    return static_cast<int>(std::floor(x));
+  }
+
+  // This way of treating Jets are potentially unsafe, hence the function name
+  template<typename Scalar, int N>
+  int PotentiallyUnsafeFloor(const ceres::Jet<Scalar, N>& x) const {
+    return static_cast<int>(std::floor(x.a));
+  };
 };
 
 template<typename T>
 class UniformR3SplineView : public SplineViewBase<T, SplineMeta> {
   using Result = std::unique_ptr<TrajectoryEvaluation<T>>;
+  using Vector3 = Eigen::Matrix<T, 3, 1>;
+  using Vector3Map = Eigen::Map<Vector3>;
  public:
   using Meta = SplineMeta;
 
   // Import constructor
   using SplineViewBase<T, SplineMeta>::SplineViewBase;
 
+  const Vector3Map ControlPoint(int i) const {
+    return Vector3Map(this->holder_->Parameter(i));
+  }
+
+  Vector3Map MutableControlPoint(int i) {
+    return Vector3Map(this->holder_->Parameter(i));
+  }
+
   Result Evaluate(T t, int flags) const override {
     auto result = std::make_unique<TrajectoryEvaluation<T>>();
+
+    int i0;
+    T u;
+    this->CalculateIndexAndInterpolationAmount(t, i0, u);
+//    std::cout << "t=" << t << " i0=" << i0 << " u=" << u << std::endl;
+
+    const size_t N = this->NumKnots();
+    if ((N < 4) || (i0 < 0) || (i0 > (N - 4))) {
+      std::stringstream ss;
+      ss << "t=" << t << " i0=" << i0 << " is out of range for spline with ncp=" << N;
+      throw std::range_error(ss.str());
+    }
+
+    const Eigen::Matrix<T, 1, 4> U(T(1), u, u*u, u*u*u);
+    const Eigen::Matrix<T, 4, 1> Q = U * M.cast<T>();
+
+    Eigen::Matrix<T, 3, 1> p = Eigen::Matrix<T, 3, 1>::Zero();
+
+    for (int i=i0; i < i0 + 4; ++i) {
+      p += Q(i - i0) * ControlPoint(i);
+    }
+
+    result->position = p;
+
     return result;
   }
 };
@@ -102,10 +153,17 @@ class UniformR3SplineTrajectory : public TrajectoryBase<detail::UniformR3SplineV
     return AsView().dt();
   }
 
+  Vector3 ControlPoint(size_t i) {
+    return AsView().ControlPoint(i);
+  }
+
+  Vector3 operator[](size_t i) {
+    return ControlPoint(i);
+  }
+
   void AppendKnot(const Vector3& cp) {
     auto i = this->holder_->AddParameter(3);
-    Eigen::Map<Vector3> cpmap(holder_->Parameter(i));
-    cpmap = cp;
+    AsView().MutableControlPoint(i) = cp;
     this->meta_.n += 1;
   }
 
@@ -118,7 +176,36 @@ class UniformR3SplineTrajectory : public TrajectoryBase<detail::UniformR3SplineV
                     Meta& meta,
                     std::vector<double*> &parameter_blocks,
                     std::vector<size_t> &parameter_sizes) const {
-    // FIXME: Do things
+    if (times.size() != 1) {
+      throw std::length_error("Mult i times not implemented yet");
+    }
+
+    int i1, i2;
+    double u_notused;
+    double t1, t2;
+
+    for (auto tt : times) {
+      t1 = tt.first;
+      t2 = tt.second;
+    }
+
+    // Find control point range
+    AsView().CalculateIndexAndInterpolationAmount(t1, i1, u_notused);
+    AsView().CalculateIndexAndInterpolationAmount(t2, i2, u_notused);
+    std::cout << "1: " << t1 << ", " << i1 << " --- 2: " << t2 << ", " << i2 << std::endl;
+
+    for (int i=i1; i < i2 + 4; ++i) {
+      auto ptr = this->holder_->Parameter(i);
+      const int size = 3;
+      parameter_blocks.push_back(ptr);
+      parameter_sizes.push_back(size);
+      problem.AddParameterBlock(ptr, size);
+    }
+
+    // Set meta
+    meta.dt = dt();
+    meta.n = (i2 + 4 - i1 + 1);
+    meta.t0 = t0() + i1 * dt();
   }
 };
 
--- src/spline_devel.cc
+++ src/spline_devel.cc
@@ -2,16 +2,58 @@
 #include <vector>
 
 #include <taser/trajectories/uniform_r3_spline_trajectory.h>
+#include <taser/trajectory_estimator.h>
+#include <taser/measurements/position_measurement.h>
 
+using namespace taser;
 using namespace taser::trajectories;
+using namespace taser::measurements;
 
 int main() {
   std::cout << "Begin" << std::endl;
 
   auto traj = std::make_shared<UniformR3SplineTrajectory>();
 
+  traj->AppendKnot(Eigen::Vector3d(1, 1, 1));
+  traj->AppendKnot(Eigen::Vector3d(1, 5, -1));
+  traj->AppendKnot(Eigen::Vector3d(2, 3, -1));
+  traj->AppendKnot(Eigen::Vector3d(1, 5, -4));
+  traj->AppendKnot(Eigen::Vector3d(-2, 12, -7));
+  traj->AppendKnot(Eigen::Vector3d(-3, 10, -8));
+
   std::cout << "dt=" << traj->dt() << ", t0=" << traj->t0();
   std::cout << " nknots=" << traj->NumKnots() << std::endl;
-  std::cout << "End OK" << std::endl;
+
+  std::cout << "::KNOTS" << std::endl;
+  for (int i=0; i < traj->NumKnots(); ++i) {
+    std::cout << i << ": " << (*traj)[i].transpose() << std::endl;
+  }
+
+  auto m1 = std::make_shared<PositionMeasurement>(0.1, Eigen::Vector3d(1, 1, 2));
+  auto m2 = std::make_shared<PositionMeasurement>(1.2, Eigen::Vector3d(3, -1, -2));
+  auto m3 = std::make_shared<PositionMeasurement>(1.8, Eigen::Vector3d(3.2, -1.4, -2.2));
+  auto m4 = std::make_shared<PositionMeasurement>(2.2, Eigen::Vector3d(2.6, -1.1, -1.0));
+
+  std::vector<std::shared_ptr<PositionMeasurement>> meas = {m1, m2, m3, m4};
+
+  TrajectoryEstimator<UniformR3SplineTrajectory> estimator(traj);
+  for (auto m : meas){
+    estimator.AddMeasurement(m);
+  }
+
+  auto summary = estimator.Solve();
+
+  std::cout << summary.FullReport() << std::endl;
+
+  for (auto m : meas){
+    Eigen::Vector3d phat = m->Measure<double, UniformR3SplineTrajectory>(traj->AsView());
+    std::cout << "t=" << m->t << " p=" << m->p.transpose() << "  -->  " << phat.transpose() << std::endl;
+  }
+
+  std::cout << "::KNOTS" << std::endl;
+  for (int i=0; i < traj->NumKnots(); ++i) {
+    std::cout << i << ": " << (*traj)[i].transpose() << std::endl;
+  }
+
   return 0;
 }
