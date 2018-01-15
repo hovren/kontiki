a94499ab70b127135f82351c2c03f7a33395e2e4
Hannes Ovrén <hannes.ovren@liu.se>
2018-01-15 15:38:54 +0100

    Tests for position using SciPy spline implementation

--- tests/trajectories/test_r3spline_trajectory.py
+++ tests/trajectories/test_r3spline_trajectory.py
@@ -1,7 +1,19 @@
 import pytest
 import numpy as np
+from scipy.interpolate import BSpline
 
 from taser.trajectories import UniformR3SplineTrajectory
+
+
+def scipy_bspline(control_points, dt=1, k=3, t0=0):
+    n = len(control_points)
+    Nt = k + n + 1
+    t = dt * (np.arange(Nt) - k) + t0
+    return BSpline(t, control_points, k, extrapolate=False)
+
+
+def scipy_bspline_valid_time_interval(bspl):
+    return bspl.t[bspl.k], bspl.t[-bspl.k - 1]
 
 
 @pytest.fixture
@@ -9,7 +21,10 @@ def random_r3_spline():
     N = 10
     control_points = np.random.uniform(-5, 5, size=(N, 3))
 
-    traj = UniformR3SplineTrajectory()
+    dt = np.random.uniform(0.1, 2.0)
+    t0 = np.random.uniform(-2, 2)
+
+    traj = UniformR3SplineTrajectory(dt, t0)
     for cp in control_points:
         traj.append_knot(cp)
     return traj, control_points
@@ -22,16 +37,22 @@ def test_construct_default():
     assert len(traj) == 0
 
 
-@pytest.mark.parametrize('args',[
+@pytest.mark.parametrize('dt,t0',[
     (1.0, 0.0),
     (0.5, 1.0),
     (0.5, -1.0),
-    (1.0, ),
-    (0.5, ),
+    (1.0, None),
+    (0.5, None),
 ])
-def test_construct_params_ok(args):
-    traj = UniformR3SplineTrajectory(*args)
+def test_construct_params_ok(dt, t0):
+    if t0 is None:
+        traj = UniformR3SplineTrajectory(dt)
+        t0 = 0.0  # Expected t0
+    else:
+        traj = UniformR3SplineTrajectory(dt, t0)
     assert len(traj) == 0
+    assert traj.dt == dt
+    assert traj.t0 == t0
 
 
 def test_control_points(random_r3_spline):
@@ -70,3 +91,19 @@ def test_set_control_point(random_r3_spline):
         new_cp = np.random.uniform(-5, 5, size=3)
         traj[i] = new_cp
         np.testing.assert_equal(traj[i], new_cp)
+
+
+def test_position(random_r3_spline):
+    traj, control_points = random_r3_spline
+
+    bspl_sp = scipy_bspline(control_points, dt=traj.dt, t0=traj.t0, k=3)
+
+    times = np.linspace(*scipy_bspline_valid_time_interval(bspl_sp), endpoint=False)
+
+    for t in times:
+        pos_sp = bspl_sp(t)
+        pos_our = traj.position(t)
+        print(pos_sp, pos_our, traj.dt, traj.t0)
+        np.testing.assert_almost_equal(pos_our, pos_sp)
+
+        np.testing.assert_almost_equal(vel_our, vel_sp)
